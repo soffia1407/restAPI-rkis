5f90f5dad9b891442c6b03d3e2e37ff59fe0f6f4
soffia1407 <sff140705@gmail.com>
2024-12-24 02:48:13 +0700

    add urls + models

--- library_app/models.py
+++ library_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    """Модель автора книги."""
+    name = models.CharField(max_length=100, unique=True)
+    biography = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """Модель книги."""
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    year = models.IntegerField()
+    genre = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=100)
+    cover_image = models.CharField(max_length=200)
+    text_file = models.CharField(max_length=200)
+
+    class Meta:
+        unique_together = ('title', 'author', 'year', 'publisher')
+
+    def __str__(self):
+        return self.title
