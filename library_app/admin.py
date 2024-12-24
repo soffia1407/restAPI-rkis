9ffa87bff03e68da4475399b690ebf6ccaab1a94
soffia1407 <sff140705@gmail.com>
2024-12-24 17:22:04 +0700

    add settings for admin + creat superuser

--- library_app/admin.py
+++ library_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Author, Book
 
+class BookAdmin(admin.ModelAdmin):
+  list_display = ('title', 'author', 'year', 'genre') # Указываем какие поля будут в списке
+
 admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
