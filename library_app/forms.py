ccd318a346b21c7eaff4792c9dbff05bf64988e9
soffia1407 <sff140705@gmail.com>
2024-12-24 16:57:41 +0700

    The ability to add books for users has been removed. the page and the form for adding the book were deleted.

--- library_app/forms.py
+++ library_app/forms.py
@@ -1,18 +1,2 @@
 from django import forms
 from .models import Author, Book
-
-class BookForm(forms.ModelForm):
-    author_name = forms.CharField(max_length=100, required=True, label='Автор')
-
-    class Meta:
-        model = Book
-        fields = ['title', 'year', 'genre', 'category', 'publisher', 'cover_image', 'text_file']
-        labels = {
-            'title': 'Название',
-            'year': 'Год',
-            'genre': 'Жанр',
-            'category': 'Категория',
-            'publisher': 'Издательство',
-            'cover_image': 'Обложка',  # Изменяем лейблы
-            'text_file': 'Файл с текстом',
-        }
--- library_app/views.py
+++ library_app/views.py
@@ -1,45 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404 # Функции для работы с редиректами, шаблонами и 404
 from .models import Book, Author # Импорт наших моделей
-from .forms import BookForm  # Импорт нашей формы
 from django.db.models import Q  # Импорт Q для сложных запросов
-from django.conf import settings  # Для работы с настройками проекта
-from django.core.files.storage import FileSystemStorage # Для работы с файловой системой
 
 def index(request):
     """Главная страница со списком книг."""
     books = Book.objects.all() # Получаем все книги
     return render(request, 'library_app/index.html', {'books': books}) # Рендерим шаблон
-
-def add_book(request):
-    """Страница добавления книги."""
-    if request.method == 'POST': # Если форма отправлена методом POST
-        form = BookForm(request.POST, request.FILES)  # Создаем форму, передаем данные формы и файлы
-        if form.is_valid():  # Проверяем валидность формы
-            # Получаем данные из формы (текстовые поля)
-            title = form.cleaned_data['title']
-            author_name = form.cleaned_data['author_name']
-            year = form.cleaned_data['year']
-            genre = form.cleaned_data['genre']
-            category = form.cleaned_data['category']
-            publisher = form.cleaned_data['publisher']
-            cover_image = request.FILES.get('cover_image')  # Получаем файл обложки из request.FILES
-            text_file = request.FILES.get('text_file') # Получаем файл с текстом из request.FILES
-
-            author, created = Author.objects.get_or_create(name=author_name)  # Получаем или создаем автора
-            book = Book(title=title, author=author, year=year, genre=genre, category=category, publisher=publisher,
-                        cover_image=cover_image, text_file=text_file) # Создаем книгу
-
-            try:
-                book.full_clean() # Выполняем полную валидацию модели
-                book.save()
-            except Exception as e: # Если есть ошибка при сохранении
-                form.add_error(None, f"Ошибка при добавлении книги: {e}") # Добавляем ошибку в форму
-                return render(request, 'library_app/add_book.html', {'form': form}) # Возвращаем форму с ошибкой
-
-            return redirect('index')  # Перенаправляем на главную страницу
-    else:
-        form = BookForm()  # Если GET, создаем пустую форму
-    return render(request, 'library_app/add_book.html', {'form': form}) # Отображаем форму
 
 def book_detail(request, book_id):
     """Страница с информацией о книге."""
@@ -61,9 +27,9 @@ def search(request):
     query = request.GET.get('query', '').lower() # Получаем поисковый запрос, переводим в нижний регистр
     if query:
         books = Book.objects.filter(
-            Q(title__icontains=query) |  # Поиск по названию
-            Q(genre__icontains=query) |  # Поиск по жанру
-            Q(author__name__icontains=query)  # Поиск по имени автора
+            Q(title__icontains=query) |
+            Q(genre__icontains=query) |
+            Q(author__name__icontains=query)
         ) # Фильтруем книги по запросу
     else:
         books = []  # Если нет запроса, то ничего не ищем
